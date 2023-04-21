614489026f6d560a7f78bb94bbc72acbb4d01a32
Jonathan Osmond <jonathancosmond@gmail.com>
2023-04-21 16:58:14 -0400

    fix footer copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,8 +7,8 @@ export default function Footer() {
     <footer className="mt-10 bg-secondary">
       <p className="w-full px-3 pt-8 mx-auto text-sm text-center text-gray-400 sm:order-first sm:mt-0">
         Crafted using
-        <A href="https://nextjs.org"> Next.js </A>. and
-        <A href="http://hyperui.dev"> Hyper UI </A>. Theme loosely based on <A href="http://github.com/ayu-theme">Ayu</A>
+        <A href="https://nextjs.org"> Next.js </A> and
+        <A href="http://hyperui.dev"> Hyper UI </A>. Theme loosely based on <A href="http://github.com/ayu-theme">Ayu.</A>
       </p>
       <p className="text-xs text-gray-700 opacity-30">
         {" Pineapple on pizza is an atrocity, don't @ me."}
