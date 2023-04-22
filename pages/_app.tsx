c7d78737fdebb1209b7e6eb525b951fe2243beb1
Jonathan Osmond <jonathancosmond@gmail.com>
2023-04-21 20:37:42 -0400

    update meta tags, favicon and open graph stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
-import AlertBar from '@/components/AlertBar'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Martian_Mono } from 'next/font/google'
-const martian_mono = Martian_Mono({ variable: '--font-martian-mono', subsets: ['latin'] })
+import AlertBar from "@/components/AlertBar"
+import Footer from "@/components/Footer"
+import Header from "@/components/Header"
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import { Martian_Mono } from "next/font/google"
+import Head from "next/head"
+
+const martian_mono = Martian_Mono({
+  variable: "--font-martian-mono",
+  subsets: ["latin"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={martian_mono.variable}>
+      <Head>
+        <title>{"Jon's Portfolio"}</title>
+        <meta
+          property="og:title"
+          content="Jonathan Osmond | Software Developer Seeking Employment"
+        ></meta>
+        <meta
+          property="og:description"
+          content="Jonathan Osmond's portfolio containing a variety of projects ranging from application to game development!"
+        ></meta>
+      </Head>
       <AlertBar />
       <Header />
       <Component className={martian_mono.variable} {...pageProps} />
